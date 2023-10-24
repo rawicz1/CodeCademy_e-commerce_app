360538eae2881dd5f0cbb6c42fbbfd581c6c060b
Raf <rawiczhub@gmail.com>
2023-10-24 10:28:47 +0100

    PORT settings

--- server/server.js
+++ server/server.js
@@ -126,5 +126,7 @@ const app = require('./app')
 //     }
 // })
 
-
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+// app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
