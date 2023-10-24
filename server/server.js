df3b4226bb67eac19d3271a0a9d2c6f141d05800
Raf <rawiczhub@gmail.com>
2023-10-24 10:30:31 +0100

    PORT 8000

--- server/server.js
+++ server/server.js
@@ -126,7 +126,7 @@ const app = require('./app')
 //     }
 // })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 // app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
